9cf0194612b8ab5ad8d08feb99f92213117994de
unknown <deofarady43@gmail.com>
2024-11-04 00:26:18 +0700

    feat: add week-6 assigment

---
+++ [06] Minggu ke-6/tugas_6/lib/homepage.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  final List<String> _items = ["Item 1", "Item 2", "Item 3"];
+
+  void _showDetailDialog(String itemName) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(itemName),
+          content: Text('Detail untuk $itemName'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // Menutup dialog
+              },
+              child: const Text('Close'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        // appbar
+        appBar: AppBar(
+          title: const Text(
+            "Tugas 6",
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+          backgroundColor: Colors.blueAccent,
+        ),
+
+        //body
+        body: ListView.builder(
+          padding: const EdgeInsets.all(20),
+          itemCount: _items.length,
+          itemBuilder: (context, index) {
+            return GestureDetector(
+              onTap: () => _showDetailDialog(_items[index]),
+              child: Container(
+                padding: const EdgeInsets.all(10),
+                child: Row(
+                  children: [
+                    Container(
+                      height: 32,
+                      width: 32,
+                      decoration: const BoxDecoration(
+                        color: Colors.black54,
+                        shape: BoxShape.circle,
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    Text(
+                      _items[index],
+                      style: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
