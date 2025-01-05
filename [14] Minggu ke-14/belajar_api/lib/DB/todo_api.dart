a2193abde4b703d1503e031a36c89463b7d0caa9
DeoFaradyS <deofarady43@gmail.com>
2025-01-06 02:15:24 +0700

    feat: Refactor TodoApi class to improve API request handling and response management

--- [14] Minggu ke-14/belajar_api/lib/DB/todo_api.dart
+++ [14] Minggu ke-14/belajar_api/lib/DB/todo_api.dart
@@ -1,62 +1,59 @@
+import 'dart:convert';
 import 'package:belajar_api/Model/todo_api_model.dart';
 import 'package:http/http.dart' as http;
 
-String baseURL = 'https://6767d3e0c1de2e6421c85934.mockapi.io/todo';
-var client = http.Client();
-
 class TodoApi {
-  Future<dynamic> getTodo() async {
-    var url = Uri.parse(baseURL);
-    var response = await client.get(url);
-    if (response.statusCode == 200) {
-      return todoApiModelFromJson(response.body);
-    } else {
-      return null;
-    }
+  static const String _baseURL = 'https://6767d3e0c1de2e6421c85934.mockapi.io/todo';
+  final http.Client _client;
+
+  TodoApi({http.Client? client}) : _client = client ?? http.Client();
+
+  Future<List<TodoApiModel>> fetchTodos() async {
+    final response = await _getRequest(_baseURL);
+    return _handleResponse<List<TodoApiModel>>(response, (data) => (jsonDecode(data) as List).map((item) => TodoApiModel.fromJson(item)).toList());
   }
 
-  Future<dynamic> getById(String id) async {
-    var url = Uri.parse('$baseURL/$id');
-    var response = await client.get(url);
-    if (response.statusCode == 200) {
-      return todoApiModelFromJson(response.body);
-    } else {
-      return null;
-    }
+  Future<TodoApiModel> fetchTodoById(String id) async {
+    final response = await _getRequest('$_baseURL/$id');
+    return _handleResponse<TodoApiModel>(response, (data) => TodoApiModel.fromJson(jsonDecode(data)));
   }
 
-  Future<dynamic> post(String title) async {
-    var url = Uri.parse(baseURL);
-    var body = {"title": title};
-    var response = await client.post(url, body: body);
-
-    if (response.statusCode == 201) {
-      return response.body;
-    } else {
-      return null;
-    }
+  Future<TodoApiModel> createTodo(String title) async {
+    final response = await _postRequest(_baseURL, {'title': title});
+    return _handleResponse<TodoApiModel>(response, (data) => TodoApiModel.fromJson(jsonDecode(data)));
   }
 
-  Future<dynamic> put(String id, String title) async {
-    var url = Uri.parse('$baseURL/$id');
-    var body = {"title": title};
-    var response = await client.put(url, body: body);
-
-    if (response.statusCode == 200) {
-      return response.body;
-    } else {
-      return null;
-    }
+  Future<TodoApiModel> updateTodo(String id, String title) async {
+    final response = await _putRequest('$_baseURL/$id', {'title': title});
+    return _handleResponse<TodoApiModel>(response, (data) => TodoApiModel.fromJson(jsonDecode(data)));
   }
 
-  Future<dynamic> delete(String id) async {
-    var url = Uri.parse('$baseURL/$id');
-    var response = await client.delete(url);
+  Future<void> deleteTodo(String id) async {
+    final response = await _deleteRequest('$_baseURL/$id');
+    _handleResponse<void>(response, (data) {});
+  }
 
-    if (response.statusCode == 200) {
-      return response.body;
+  Future<http.Response> _getRequest(String url) async {
+    return await _client.get(Uri.parse(url));
+  }
+
+  Future<http.Response> _postRequest(String url, Map<String, String> body) async {
+    return await _client.post(Uri.parse(url), body: jsonEncode(body), headers: {'Content-Type': 'application/json'});
+  }
+
+  Future<http.Response> _putRequest(String url, Map<String, String> body) async {
+    return await _client.put(Uri.parse(url), body: jsonEncode(body), headers: {'Content-Type': 'application/json'});
+  }
+
+  Future<http.Response> _deleteRequest(String url) async {
+    return await _client.delete(Uri.parse(url));
+  }
+
+  T _handleResponse<T>(http.Response response, T Function(String) fromJson) {
+    if (response.statusCode >= 200 && response.statusCode < 300) {
+      return fromJson(response.body);
     } else {
-      return null;
+      throw Exception('Failed to load data: ${response.statusCode}');
     }
   }
 }
