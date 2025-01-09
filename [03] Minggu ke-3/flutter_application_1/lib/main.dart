dddc234f45feab7a131dcf7722f9cac63e1b32e1
DeoFaradyS <deofarady43@gmail.com>
2025-01-09 22:59:18 +0700

    feat: Update analysis_options.yaml to comment out lints inclusion; clean up main.dart imports

--- [03] Minggu ke-3/flutter_application_1/lib/main.dart
+++ [03] Minggu ke-3/flutter_application_1/lib/main.dart
@@ -1,8 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/%5B03%5D%20columnn.dart';
-import 'package:flutter_application_1/%5B01%5D%20container.dart';
-import 'package:flutter_application_1/%5B04%5D%20ListVieww.dart';
-import 'package:flutter_application_1/%5B05%5D%20GridVieww.dart';
 import 'package:flutter_application_1/%5B06%5D%20Stack.dart';
 
 void main() {
